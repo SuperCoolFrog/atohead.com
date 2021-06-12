0b6f93313a937ac2ec4c69e6118e0ef5458f6d8c
Michael Vasquez <michael.vasquez@vertexinc.com>
2021-06-11 23:00:59 -0400

    Add jumbotron

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
 import GameCard from '../../Components/GameCard/GameCard';
 import { default as GameCardModel } from '../../Models/GameCard';
 import './Home.scss';
 
 function App() {
-  const [cards, setCards] = useState<GameCardModel[]>([]);
+  // const [cards, setCards] = useState<GameCardModel[]>([]);
   
-  useEffect(() => {
+  // useEffect(() => {
     
-    fetch('/game-data/cards.json')
-    .then(response => response.json())
-    .then(data => { 
-      console.log(data)
-      setCards(data.cards);
-    });
+  //   fetch('/game-data/cards.json')
+  //   .then(response => response.json())
+  //   .then(data => { 
+  //     console.log(data)
+  //     setCards(data.cards);
+  //   });
     
-  }, []);
+  // }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      {cards.map((card) => (
-        <GameCard card={card} key={card.characterType + card.serialNumber}></GameCard>
-      ))}
-    </div>
-  );
+  return (<>
+    <Jumbotron>
+      <Container>
+        <h1>Across the Obelisk - Tools and Guides</h1>
+        <p>
+          gghf
+        </p>
+      </Container>
+    </Jumbotron>
+  </>);
 }
 
 export default App;
