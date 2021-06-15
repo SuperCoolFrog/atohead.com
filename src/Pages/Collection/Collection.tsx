6451fd2ca2d4311defb23373a0e96a55c3ab29ae
Michael Vasquez <michael.vasquez@vertexinc.com>
2021-06-15 00:40:24 -0400

    Padding and margins collection page

--- src/Pages/Collection/Collection.tsx
+++ src/Pages/Collection/Collection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { getCards } from '../../API/API';
 import GameCard from '../../Components/SmallGameCard/SmallGameCard';
@@ -10,22 +10,60 @@ const Collection = () => {
     const [cards, setCards] = useState<GameCardModel[]>([]);
     const [filteredCards, setFilteredCards] = useState<GameCardModel[]>([]);
 
+    const containerRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
         getCards().then((cards) => {
             setCards(cards);
             setFilteredCards(cards);
         });
     }, []);
+
+    // useEffect(() => {
+    //     function setFirstClass(){
+    //         let children = containerRef?.current?.children;
+    //         let firstChild = containerRef?.current?.firstElementChild;
+    //         let leftPosition: number = -1;
+
+    //         if (!(firstChild && children)) {
+    //             return;
+    //         }
+            
+    //         leftPosition = (firstChild as HTMLDivElement).offsetLeft;
+
+    //         Object.keys(children)
+    //             .map(k => parseInt(k))
+    //             .forEach(k => {
+    //                 let ele = (containerRef?.current?.children[k] as HTMLDivElement);
+    //                 if (ele) {
+    //                     if (ele.offsetLeft <= leftPosition){
+    //                         ele.classList.add('firstColumn');
+    //                     } else {
+    //                         ele.classList.remove('firstColumn');
+    //                     }
+    //                 }
+    //             });
+    //     }
+    //     window.addEventListener('resize', setFirstClass);
+    //     setFirstClass();
+    // }, []);
     
     return (<Container>
         <h2>Warrior</h2>
-        <Row>
+        <div id="collectionCardContainer" className={styles.collectionRow} ref={containerRef}>
+            {filteredCards.filter(c => c.characterType === CharacterType.WARRIOR).map((card) => (
+                // <div className={styles.cardContainer} key={card.characterType + card.serialNumber}>
+                    <GameCard card={card} key={card.characterType + card.serialNumber} />
+                // </div>
+            ))}
+        </div>
+        {/* <Row>
             {filteredCards.filter(c => c.characterType === CharacterType.WARRIOR).map((card) => (
                 <Col className={styles.cardContainer} key={card.characterType + card.serialNumber}>
                     <GameCard card={card} />
                 </Col>
             ))}
-        </Row>
+        </Row> */}
     </Container>);
     
 };
