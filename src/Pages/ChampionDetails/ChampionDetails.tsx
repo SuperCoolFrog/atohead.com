7f25bc5da80a8ad567d990a3fc3f923a90632d01
Michael Vasquez <michael.vasquez@vertexinc.com>
2021-06-26 02:55:23 -0400

    Add breadcrumb to details

--- src/Pages/ChampionDetails/ChampionDetails.tsx
+++ src/Pages/ChampionDetails/ChampionDetails.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Breadcrumb, Container } from 'react-bootstrap';
 import styles from './champion-details.module.scss';
 import Details from '../../Components/Details/Details';
 import {
+    Link,
     useParams
 } from "react-router-dom";
 import API from '../../API/API';
@@ -23,11 +24,19 @@ const ChampionDetails = () => {
         });
     }, [])
 
-    return (<Container className={styles.container}>
-        {character && (
-            <Details character={character} />
-        )}
-    </Container>);
+    return (<>
+        <Container className={styles.container}>
+            <Container>
+                <Breadcrumb>
+                    <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/champions" }}>Champions</Breadcrumb.Item>
+                    <Breadcrumb.Item active>{character?.name || ''}</Breadcrumb.Item>
+                </Breadcrumb>
+            </Container>
+            {character && (
+                <Details character={character} />
+            )}
+        </Container>
+    </>);
 };
 
 export default ChampionDetails;
